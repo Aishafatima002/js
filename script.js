a617220f433358e90d3a316f1312d1cfb6a62919
Aisha <164768280+Aishafatima002@users.noreply.github.com>
2024-05-15 14:44:26 +0500

    update

---
+++ script.js
@@ -0,0 +1,79 @@
+// a++//post increament
+// ++a//preincreament
+// let a=10;
+// // console.log(++a);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+// let number1 = parseInt(prompt("Enter your number1",20));
+// let number2=parseInt(prompt("Enter your number2"));
+// console.log(number1 + number2);
+
+
+// let y=12;
+// letx=14;
+// console.log(y!==z);
+
+// let a=20;
+// a+=3;
+// console.log(a)
+
+// let mode="light";
+// let color="red";
+// if(mode=="dark")
+//  {console.log(color)}
+// else{
+//     console.log("white")
+// };
+
+// let x=prompt("enter your age");
+// if(x>15){
+//     console.log("you are elgibille");
+// }
+// else{
+//     console.log("you are not ellgible")
+// };
+// let name=prompt("your name");
+// let b="aisha"
+// if(name===b){
+//     console.log ("yes")
+// }
+// else{
+//     console.log("no you not!")
+
+
+// let number=prompt("enter your number");
+// if(number%2==0){
+//     console.log(number,"is aeven number");
+// }
+// else{
+//     console.log(number,"it is a odd number")
+// }
+// let b= prompt("what it is");
+// let correctanswer="vacation";
+// if( b===correctanswer ){
+//     alert("correct!"
+//     );
+// }
+// else{
+//     console.log("not correct!")
+// };
+// let age=+prompt("enter your age");
+// if(age<13){
+//     console.log("junior");
+// }
+// else if(age>60){
+//     console.log("senior");
+// }
+// else{
+//     console.log("middle")
+// };
+
+// let number=prompt("enter your number");
+// if(number%5==0){
+//     console.log(number,"is multiple of 5")
+// }
+// else{
+//     console.log(number,"it is not multiple of 5")
+// };
